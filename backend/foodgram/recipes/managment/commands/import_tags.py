bcd83ee194f019112b46e0d65dc2d12e129ecea4
Vladimir <vdnikulin24@gmail.com>
2023-07-26 02:04:50 +0500

    Done

--- backend/foodgram/recipes/managment/commands/import_tags.py
+++ backend/foodgram/recipes/managment/commands/import_tags.py
@@ -3,22 +3,23 @@ import csv
 from django.conf import settings
 from django.core.management.base import BaseCommand
 from django.db import IntegrityError
-from recipes.models import Ingredient
+from recipes.models import Tag
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
         with open(
-            f'{settings.BASE_DIR}/data/ingredients.csv',
+            f'{settings.BASE_DIR}/data/tags.csv',
             'r',
             encoding='utf-8'
         ) as csv_file:
             reader = csv.DictReader(csv_file)
             try:
-                Ingredient.objects.bulk_create(
-                    Ingredient(**items) for items in reader
+                Tag.objects.bulk_create(
+                    Tag(**items) for items in reader
                 )
             except IntegrityError:
-                return 'Такие ингредиенты уже есть...'
-        return (
-            f'{Ingredient.objects.count()} - ингредиентов успешно загружено')
+                return 'Такие Теги уже есть...'
+        return (f'Теги успешно загружены -> '
+                f'{(", ").join([_.get("name") for _ in Tag.objects.values()])}'
+                )
