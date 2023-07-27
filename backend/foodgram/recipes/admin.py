ad0887bff1b13325e7763c80b2ba8654a5b4fd44
Vladimir <vdnikulin24@gmail.com>
2023-07-27 16:21:22 +0500

    Done

--- backend/foodgram/recipes/admin.py
+++ backend/foodgram/recipes/admin.py
@@ -1,44 +1,51 @@
 from django.contrib import admin
 
-from recipes.models import (AmountOfIngredient, FavouriteRecipe,
-                            Ingredient, Recipe, ShoppingCart, Tag)
+from recipes.models import (Favorite, Ingredient, Recipe,
+                            RecipeIngredient, ShoppingCart, Tag)
 
 
 @admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
     list_display = ('pk', 'name', 'color', 'slug')
     search_fields = ('name', 'color', 'slug')
+    list_filter = ('name', 'color', 'slug')
     empty_value_display = '-пусто-'
 
 
 @admin.register(Ingredient)
 class IngredientAdmin(admin.ModelAdmin):
     list_display = ('pk', 'name', 'measurement_unit')
-    list_filter = ('name', )
-    search_fields = ('name', )
+    search_fields = ('name',)
+    list_filter = ('name',)
     empty_value_display = '-пусто-'
+
+
+class RecipeIngredientInline(admin.TabularInline):
+    model = RecipeIngredient
 
 
 @admin.register(Recipe)
 class RecipeAdmin(admin.ModelAdmin):
-    list_display = ('pk', 'name', 'author', 'favourites_amount')
-    readonly_fields = ('favourites_amount',)
+    list_display = ('pk', 'name', 'author', 'favorites_amount')
+    search_fields = ('name', 'author')
     list_filter = ('name', 'author', 'tags')
     empty_value_display = '-пусто-'
+    inlines = [
+        RecipeIngredientInline,
+    ]
 
-    @admin.display(description='В избранном')
-    def favourites_amount(self, obj):
-        return obj.favourite.count()
+    def favorites_amount(self, obj):
+        return obj.favorites.count()
 
 
-@admin.register(AmountOfIngredient)
-class AmountOfIngredientAdmin(admin.ModelAdmin):
+@admin.register(RecipeIngredient)
+class RecipeIngredientAdmin(admin.ModelAdmin):
     list_display = ('pk', 'recipe', 'ingredient', 'amount')
     empty_value_display = '-пусто-'
 
 
-@admin.register(FavouriteRecipe)
-class FavouriteRecipeAdmin(admin.ModelAdmin):
+@admin.register(Favorite)
+class FavoriteAdmin(admin.ModelAdmin):
     list_display = ('pk', 'user', 'recipe')
     search_fields = ('user', 'recipe')
     empty_value_display = '-пусто-'
--- backend/foodgram/users/admin.py
+++ backend/foodgram/users/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import User, Subscribe
+from .models import User, Subscription
 
 
 @admin.register(User)
@@ -14,7 +14,7 @@ class UserAdmin(admin.ModelAdmin):
     empty_value_display = '-пусто-'
 
 
-@admin.register(Subscribe)
+@admin.register(Subscription)
 class SubscribeAdmin(admin.ModelAdmin):
     list_display = ('pk', 'user', 'author')
     search_fields = ('user', 'author')
