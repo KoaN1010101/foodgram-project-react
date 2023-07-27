e00fc545fc60e8c27f08568d17dfde6a9f7bd1b4
Vladimir <vdnikulin24@gmail.com>
2023-07-28 04:35:56 +0500

    Done

--- backend/foodgram/api/utils.py
+++ backend/foodgram/api/utils.py
@@ -45,12 +45,3 @@ def creating_an_ingredient(ingredients, recipe):
             )
         )
     AmountOfIngredient.objects.bulk_create(ingredient_list)
-
-
-class Base64ImageField(ImageField):
-    def to_internal_value(self, data):
-        if isinstance(data, str) and data.startswith('data:image'):
-            format, imgstr = data.split(';base64')
-            ext = format.split('/')[-1]
-            data = ContentFile(b64decode(imgstr), name='photo.' + ext)
-        return super().to_internal_value(data)
